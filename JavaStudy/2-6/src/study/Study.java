4f95246ce8e98df7d02000de45dbfa7b3fceae6f
kkh456 <kkta0710@icloud.com>
2020-12-17 22:04:35 +0900

    Task2-6の課題再提出です
    Task2-6の再提出課題を解きました

--- JavaStudy/2-6/src/study/Study.java
+++ JavaStudy/2-6/src/study/Study.java
@@ -28,7 +28,8 @@ public class Study {
 
         // 問② 「(Calendar.MONTH) + 数値」の処理は、想定していない値となる可能性があります。
         // その理由をコメントへ記述してください。
-        // [ここへ記述]100で割り切れて、400で割り切れるうるう年がある為、想定していない値の場合はうるう年のチェックする
+        // [ここへ記述]1月の実値の値が０のため、12月が実値11である。そのため−1の値を保持しているため＋1などで調整しているが
+        //繰り上げ繰り下げの場合は月の値に＋1するため13月、14月などが表示されるため
         System.out.println(calendar.get(Calendar.YEAR));
         System.out.println(calendar.get(Calendar.MONTH) + 1);
         System.out.println(calendar.get(Calendar.DATE));
@@ -39,9 +40,9 @@ public class Study {
         calendar.add(Calendar.MONTH,2);
         calendar.add(Calendar.DATE,15);
         System.out.println(calendar.get(Calendar.YEAR));
-        System.out.println(calendar.get(Calendar.MONTH));
+        System.out.println(calendar.get(Calendar.MONTH)+1);
         System.out.println(calendar.get(Calendar.DATE));
-        
+
 
 
 
