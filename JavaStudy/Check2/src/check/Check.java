288c0ab7a1b8de91422d9375ee2354b681b71be4
kkh456 <kkta0710@icloud.com>
2021-01-06 22:55:46 +0900

    Check2の課題提出です
    Check2の課題を解きました

---
+++ JavaStudy/Check2/src/check/Check.java
@@ -0,0 +1,26 @@
+package check;
+import constants.Constants;
+
+public class Check {
+	 private static String firstName="粂";
+	 private static String lastName="光志郎";
+
+	public static void main(String[] args) {
+		
+		Pet pet =new Pet(Constants.CHECK_CLASS_JAVA,Constants.CHECK_CLASS_HOGE);
+		RobotPet robotPet=new RobotPet(Constants.CHECK_CLASS_R2D2,Constants.CHECK_CLASS_LUKE);
+		
+		printName();
+		pet.introduce();
+		robotPet.introduce();
+ 	}
+
+	public static void printName(){
+		System.out.println("printNameメソッド→"+firstName+lastName);
+	}
+
+//	private static String printName() {
+//		System.out.println("printNameメソッド→"+firstName+lastName);
+//		return printName();
+//	}
+}
