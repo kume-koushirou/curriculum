afef6245d4853cd35431c021a2d58f3b326f34b5
kkh456 <kkta0710@icloud.com>
2021-05-13 23:19:12 +0900

    1-3再提出

--- JavaScript/1-4/main.js
+++ JavaScript/1-4/main.js
@@ -1,14 +1,14 @@
 for(let i = 1; i <= 100; i++){
-  //iが3の倍数かつ5の倍数のときにFizzBuzzと出力
+  
   if(i % 3 === 0 && i % 5 ===0){
       console.log("FizzBuzz!!");
-  //iが3の倍数の場合Fizzと出力
+  
   } else if (i % 5 === 0){
       console.log("Buzz!");
-  //iが5の倍数の場合buzzと出力
+  
  } else if (i % 3 === 0){
       console.log("Fizz");
-//数値iが上記の条件以外の数値の場合数値を返す
+
   } else{
     console.log(i);
   }
