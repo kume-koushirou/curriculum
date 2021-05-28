b3fbb68cd6c4b6ab3a168b241fe9bf979432fe7a
kkh456 <kkta0710@icloud.com>
2021-05-28 20:29:36 +0900

    1-6課題提出
    1-6課題解きました

---
+++ JavaScript/1-6/main.js
@@ -0,0 +1,15 @@
+let scores = [10,15,20,25];
+for(let i=0; i<scores.length; i++){
+  if (scores[i] % 2 === 0) {
+    console.log(scores[i]+"は偶数です。");
+  }
+ 
+}
+
+let gasoline= {
+  gass:"20.5",
+  num:1234
+};
+
+console.log("ガソリンは"+gasoline.gass+"です");
+console.log("ナンバーは"+gasoline.num+"です");
